ecea1462d6b5c6895f68c92965d6aec74f752bb2
ThoughtfulAI <contact@thoughtfulai.blog>
2025-03-23 16:42:22 +0000

    검색 오류 해결: Liquid 템플릿 문법 수정 및 스크립트 로딩 개선

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,21 +3,22 @@
 ---
 
 var store = [
-    {% - for c in site.collections -%}
-{% - if forloop.last -%}
-{% - assign l = true -%}
-{% - endif -%}
-{% - assign docs = c.docs | where_exp: 'doc', 'doc.search != false' -%}
-{% - for doc in docs -%}
-{% - if doc.header.teaser -%}
-{% - capture teaser -%} { { doc.header.teaser } } {% - endcapture -%}
-{% - else -%}
-{% - assign teaser = site.teaser -%}
-{% - endif -%}
+    {% assign docs = site.collections %}
+{% for c in docs %}
+{% if forloop.last %}
+{% assign l = true %}
+{% endif %}
+{% assign docs = c.docs | where_exp: 'doc', 'doc.search != false' %}
+{% for doc in docs %}
+{% if doc.header.teaser %}
+{% capture teaser %} { { doc.header.teaser } } {% endcapture %}
+{% else %}
+{% assign teaser = site.teaser %}
+{% endif %}
 {
     "title": { { doc.title | jsonify } },
     "excerpt":
-    {% - if site.search_full_content == true -%}
+    {% if site.search_full_content == true %}
     {
         {
             doc.content | newline_to_br |
@@ -31,8 +32,8 @@ var store = [
                                         replace: "</h6>", " " |
                                             strip_html | strip_newlines | jsonify
         }
-    },
-    {% - else -%}
+    }
+    {% else %}
     {
         {
             doc.content | newline_to_br |
@@ -46,29 +47,30 @@ var store = [
                                         replace: "</h6>", " " |
                                             strip_html | strip_newlines | truncatewords: 50 | jsonify
         }
-    },
-    {% - endif -%}
+    }
+    {% endif %},
     "categories": { { doc.categories | jsonify } },
     "tags": { { doc.tags | jsonify } },
     "url": { { doc.url | absolute_url | jsonify } },
     "teaser": { { teaser | absolute_url | jsonify } }
-} {% - unless forloop.last and l -%}, {% - endunless -%}
-{% - endfor -%}
-{% - endfor -%}
+} {% unless forloop.last and l %}, {% endunless %}
+{% endfor %}
+{% endfor %}
 
-{% - for post in site.posts -%}
-{% - if forloop.last -%}
-{% - assign l = true -%}
-{% - endif -%}
-{% - if post.header.teaser -%}
-{% - capture teaser -%} { { post.header.teaser } } {% - endcapture -%}
-{% - else -%}
-{% - assign teaser = site.teaser -%}
-{% - endif -%}
+{% assign posts = site.posts | where_exp: 'post', 'post.search != false' %}
+{% for post in posts %}
+{% if forloop.last %}
+{% assign l = true %}
+{% endif %}
+{% if post.header.teaser %}
+{% capture teaser %} { { post.header.teaser } } {% endcapture %}
+{% else %}
+{% assign teaser = site.teaser %}
+{% endif %}
 {
     "title": { { post.title | jsonify } },
     "excerpt":
-    {% - if site.search_full_content == true -%}
+    {% if site.search_full_content == true %}
     {
         {
             post.content | newline_to_br |
@@ -82,8 +84,8 @@ var store = [
                                         replace: "</h6>", " " |
                                             strip_html | strip_newlines | jsonify
         }
-    },
-    {% - else -%}
+    }
+    {% else %}
     {
         {
             post.content | newline_to_br |
@@ -97,29 +99,29 @@ var store = [
                                         replace: "</h6>", " " |
                                             strip_html | strip_newlines | truncatewords: 50 | jsonify
         }
-    },
-    {% - endif -%}
+    }
+    {% endif %},
     "categories": { { post.categories | jsonify } },
     "tags": { { post.tags | jsonify } },
     "url": { { post.url | absolute_url | jsonify } },
     "teaser": { { teaser | absolute_url | jsonify } }
-} {% - unless forloop.last and l -%}, {% - endunless -%}
-{% - endfor -%}
+} {% unless forloop.last and l %}, {% endunless %}
+{% endfor %}
 
-{% - assign pages = site.pages | where_exp: 'page', 'page.search != false' -%}
-{% - for page in pages -%}
-{% - if forloop.last -%}
-{% - assign l = true -%}
-{% - endif -%}
-{% - if page.header.teaser -%}
-{% - capture teaser -%} { { page.header.teaser } } {% - endcapture -%}
-{% - else -%}
-{% - assign teaser = site.teaser -%}
-{% - endif -%}
+{% assign pages = site.pages | where_exp: 'page', 'page.search != false' %}
+{% for page in pages %}
+{% if forloop.last %}
+{% assign l = true %}
+{% endif %}
+{% if page.header.teaser %}
+{% capture teaser %} { { page.header.teaser } } {% endcapture %}
+{% else %}
+{% assign teaser = site.teaser %}
+{% endif %}
 {
     "title": { { page.title | jsonify } },
     "excerpt":
-    {% - if site.search_full_content == true -%}
+    {% if site.search_full_content == true %}
     {
         {
             page.content | newline_to_br |
@@ -133,8 +135,8 @@ var store = [
                                         replace: "</h6>", " " |
                                             strip_html | strip_newlines | jsonify
         }
-    },
-    {% - else -%}
+    }
+    {% else %}
     {
         {
             page.content | newline_to_br |
@@ -148,12 +150,12 @@ var store = [
                                         replace: "</h6>", " " |
                                             strip_html | strip_newlines | truncatewords: 50 | jsonify
         }
-    },
-    {% - endif -%}
+    }
+    {% endif %},
     "categories": { { page.categories | jsonify } },
     "tags": { { page.tags | jsonify } },
     "url": { { page.url | absolute_url | jsonify } },
     "teaser": { { teaser | absolute_url | jsonify } }
-} {% - unless forloop.last and l -%}, {% - endunless -%}
-{% - endfor -%}
+} {% unless forloop.last and l %}, {% endunless %}
+{% endfor %}
 ] 
